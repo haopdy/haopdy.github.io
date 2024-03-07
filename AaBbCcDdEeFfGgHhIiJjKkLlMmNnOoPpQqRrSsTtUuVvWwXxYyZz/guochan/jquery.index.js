920e936e18ff27893067ced10db8d7c7bbb8e2fc
haopdy <84508674+haopdy@users.noreply.github.com>
2024-03-07 18:00:48 +0800

    Update jquery.index.js

--- AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/jquery.index.js
+++ AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/jquery.index.js
@@ -1,1 +1,24 @@
-var abcd="https://chaojie.diskstation.org/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index.json", abcd1="https://chaojie.diskstation.org/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index1.json", abcd2="https://chaojie.diskstation.org/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index2.json", abcd3="https://chaojie.diskstation.org/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index3.json", abcd4="https://chaojie.diskstation.org/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index4.json";
+// 定义可能的值
+var possibleValues = [urldizhi+'/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index.json', urldizhi+'/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index1.json', urldizhi+'/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index2.json', urldizhi+'/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index3.json', urldizhi+'/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index4.json'];
+
+var abcd=urldizhi+'/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index.json', abcd1=urldizhi+'/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index1.json', abcd2=urldizhi+'/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index2.json', abcd3=urldizhi+'/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index3.json', abcd4=urldizhi+'/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/guochan/index4.json';
+
+
+// 洗牌算法函数
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// 随机替换值
+shuffleArray(possibleValues);
+
+// 随机选择新的值并替换变量的内容
+  abcd = possibleValues[0];
+  abcd1 = possibleValues[1];
+  abcd2 = possibleValues[2];
+  abcd3 = possibleValues[3]; 
+  abcd4 = possibleValues[4]; 
+  
